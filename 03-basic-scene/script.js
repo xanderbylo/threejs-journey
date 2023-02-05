0d1cc510eb644b6a08340695081a72b5179f4a0b
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-05 22:45:19 +0000

    Add scene and red cube

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,1 +1,9 @@
-console.log(THREE);
+// Scene
+
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: '#ff0000'});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
