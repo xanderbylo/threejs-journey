ab2c9e8d3947ceaecfce2808506c0a075484b748
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-06 10:00:47 +0000

    Fix scene and object

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
 
 // Scene
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: '#ff0000'});
-new mesh = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Sizes
