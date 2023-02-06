ae33223ebe294c8e2aa85482ac106d42b8fffd5c
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-06 23:47:46 +0000

    Add axesHelper

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { Vector3 } from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -6,32 +7,32 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
+// Objects
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0.7, -0.6, 1)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+// Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 1   
+camera.position.y = 1
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
