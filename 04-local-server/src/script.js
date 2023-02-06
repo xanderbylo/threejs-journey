5162ec5a15158310c1e33be83794993414bdff04
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-06 09:59:49 +0000

    Add sizes, camera and renderer

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -8,3 +8,21 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: '#ff0000'});
 new mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl'),
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
