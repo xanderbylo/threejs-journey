7ef57cf62db4fc1210112bfeca75bdb493a72f64
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-11 22:12:24 +0000

    Update object

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -11,8 +11,24 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4)
+
+const geometry = new THREE.BufferGeometry()
+
+const count = 100
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = Math.random() - 0.5
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true, 
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
