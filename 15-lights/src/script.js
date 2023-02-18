f7f56f6e6f1fb074e1778fca8e36282ef1ffc69d
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-18 23:14:58 +0000

    Update lights

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -18,13 +18,31 @@ const scene = new THREE.Scene()
  * Lights
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.set(1, 0.25, 0)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
+pointLight.position.set(1, -0.5, 1)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 6, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+spotLight.target.position.x = -0.75
+
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.01)
+gui.add(hemisphereLight, 'intensity').min(0).max(1).step(0.01)
+gui.add(pointLight, 'intensity').min(0).max(1).step(0.01)
+gui.add(rectAreaLight, 'intensity').min(0).max(1).step(0.01)
+gui.add(spotLight, 'intensity').min(0).max(1).step(0.01)
+
+scene.add(ambientLight, directionalLight, hemisphereLight, pointLight, rectAreaLight, spotLight, spotLight.target)
 
 /**
  * Objects
